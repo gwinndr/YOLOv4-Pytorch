7068de40fcb69d86be1b675ca0e1eb08702172f3
Damon Gwinn <d.gwinn2281@gmail.com>
2020-06-15 16:47:20 -0400

    Fixed mish thresholding

--- model/activations/mish.py
+++ model/activations/mish.py
@@ -1,26 +1,30 @@
 import torch
 import torch.nn as nn
 
+from utilities.constants import *
+
 class Mish(nn.Module):
     """
     ----------
-    Author: Damon Gwinn
+    Author: Damon Gwinn (gwinndr)
     ----------
     - Implementation of the Mish activation function (https://arxiv.org/ftp/arxiv/papers/1908/1908.08681.pdf)
     - Mish is not present in Pytorch as of 1.5
     ----------
     """
 
-    def __init__(self):
+    def __init__(self, beta=MISH_BETA, threshold=MISH_THRESHOLD):
         super(Mish, self).__init__()
+
+        self.softplus = nn.Softplus(beta=beta, threshold=threshold)
 
     def forward(self, x):
         """
         ----------
-        Author: Damon Gwinn
+        Author: Damon Gwinn (gwinndr)
         ----------
         - Runs mish activation on each element of the given input
         ----------
         """
 
-        return x * torch.tanh( torch.log( 1 + torch.exp(x) ) )
+        return x * torch.tanh( self.softplus(x) )
