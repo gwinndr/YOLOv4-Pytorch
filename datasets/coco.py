5a6131a130cf7e07b04e1d71dd3a7824c02df316
Damon R. Gwinn <d.gwinn2281@gmail.com>
2020-08-09 16:44:56 -0400

    Coco dataset now does augmentations

--- datasets/coco.py
+++ datasets/coco.py
@@ -3,12 +3,12 @@ from torch.utils.data import Dataset
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 import os
-import cv2
+import random
 
 from utilities.constants import *
 from utilities.devices import cpu_device
 
-from utilities.preprocessing import preprocess_image_train
+from utilities.preprocessing import preprocess_image_train, preprocess_images_mosaic
 from utilities.images import load_image, image_to_tensor
 from utilities.inferencing import inference_on_image
 from utilities.detections import detections_best_class
@@ -25,12 +25,18 @@ class CocoDataset(Dataset):
     """
 
     # __init__
-    def __init__(self, image_root, input_dim, letterbox, annotation_file=None, max_anns=1000):
+    def __init__(self, image_root, netblock, input_dim, augment=False, letterbox=False, annotation_file=None, max_anns=1000):
         self.image_root = image_root
+        self.netblock = netblock
         self.annotation_file = annotation_file
         self.input_dim = input_dim
+        self.augment = augment
         self.letterbox = letterbox
         self.max_anns = max_anns
+
+        if(augment and letterbox):
+            print("CocoDataset: ERROR: Cannot augment and letterbox at the same time. Will not letterbox.")
+            self.letterbox = False
 
         self.coco_api = COCO(self.annotation_file)
         self.img_ids = self.coco_api.getImgIds()
@@ -58,22 +64,36 @@ class CocoDataset(Dataset):
         ----------
         """
 
+        netblock = self.netblock
+
         # Image ids given by index
         img_id = self.img_ids[idx]
-        image = self.load_image_by_id(img_id)
+        image, anns = self.load_all_by_id(img_id)
 
-        anns = self.load_annotations_by_id(img_id)
+        # Mosaic half the time
+        mosaic = (netblock.mosaic and bool(random.randint(0,1)))
+        if(mosaic and self.augment):
+            i1 = image
+            a1 = anns
 
-        # Normalizing annotations
-        ow = image.shape[CV2_W_DIM]
-        oh = image.shape[CV2_H_DIM]
-        anns[..., ANN_BBOX_X1] /= ow
-        anns[..., ANN_BBOX_Y1] /= oh
-        anns[..., ANN_BBOX_X2] /= ow
-        anns[..., ANN_BBOX_Y2] /= oh
+            # Will get 3 more random images for mosaic creation
+            id2 = random.choice(self.img_ids)
+            id3 = random.choice(self.img_ids)
+            id4 = random.choice(self.img_ids)
 
-        # preprocessing
-        img_tensor, anns_partial = preprocess_image_train(image, anns, self.input_dim, self.letterbox, force_cpu=True)
+            i2, a2 = self.load_all_by_id(id2)
+            i3, a3 = self.load_all_by_id(id3)
+            i4, a4 = self.load_all_by_id(id4)
+
+            images = (i1, i2, i3, i4)
+            anns = (a1, a2, a3, a4)
+
+            img_tensor, anns_partial = preprocess_images_mosaic(images, anns, netblock, self.input_dim, force_cpu=True)
+
+        # Normal preprocessing
+        else:
+            img_tensor, anns_partial = preprocess_image_train(image, anns, self.netblock, self.input_dim,
+                                        augment=self.augment, letterbox=self.letterbox, force_cpu=True)
 
         n_anns = len(anns_partial)
         if(n_anns > self.max_anns):
@@ -85,6 +105,29 @@ class CocoDataset(Dataset):
         anns[:n_anns] = anns_partial[:n_anns]
 
         return img_tensor, anns, img_id
+
+    # load_all_by_id
+    def load_all_by_id(self, id, load_anns=True, norm_anns=True):
+        """
+        ----------
+        Author: Damon Gwinn (gwinndr)
+        ----------
+        - Loads image and corresponding annotations by id
+        - Can specify whether annotations are loaded and if they are normalized by the image
+        - Will return image and annotations. Annotations will be None if load_anns=False
+        ----------
+        """
+
+        img = self.load_image_by_id(id)
+
+        if(not load_anns):
+            anns = None
+        else:
+            anns = self.load_annotations_by_id(id)
+            if(norm_anns):
+                self.normalize_annotations(anns, img.shape[CV2_W_DIM], img.shape[CV2_H_DIM])
+
+        return img, anns
 
     # load_image_by_id
     def load_image_by_id(self, img_id):
@@ -113,7 +156,7 @@ class CocoDataset(Dataset):
         ----------
         """
 
-        ann_id = self.coco_api.getAnnIds(imgIds=img_id)
+        ann_id = self.coco_api.getAnnIds(imgIds=img_id, iscrowd=False)
         coco_anns = self.coco_api.loadAnns(ann_id)
 
         n_boxes = len(coco_anns)
@@ -142,6 +185,23 @@ class CocoDataset(Dataset):
             darknet_anns[i, ANN_BBOX_CLASS] = coco_80
 
         return darknet_anns
+
+    # normalize_annotations
+    def normalize_annotations(self, anns, w, h):
+        """
+        ----------
+        Author: Damon Gwinn (gwinndr)
+        ----------
+        - Normalizes annotations bbox coordinates in_place
+        ----------
+        """
+
+        anns[..., ANN_BBOX_X1] /= w
+        anns[..., ANN_BBOX_Y1] /= h
+        anns[..., ANN_BBOX_X2] /= w
+        anns[..., ANN_BBOX_Y2] /= h
+
+        return
 
     # resize
     def resize(self, new_dim):
